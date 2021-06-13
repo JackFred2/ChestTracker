c5f7ba77d4b87aafcabe8c79c780a9240edc9909
Jack Pearce <jack@jackf.red>
2021-06-13 19:36:48 +0100

    Fix Expanded Storage Compat

--- src/main/java/red/jackf/chesttracker/compat/ExpandedStorageHandler.java
+++ src/main/java/red/jackf/chesttracker/compat/ExpandedStorageHandler.java
@@ -3,17 +3,18 @@ package red.jackf.chesttracker.compat;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import ninjaphenix.expandedstorage.common.block.ChestBlock;
-import ninjaphenix.expandedstorage.common.misc.CursedChestType;
+import ninjaphenix.expandedstorage.base.internal_api.block.AbstractChestBlock;
+import ninjaphenix.expandedstorage.base.internal_api.block.misc.CursedChestType;
 
 import java.util.Collection;
 import java.util.Collections;
 
+@SuppressWarnings("UnstableApiUsage")
 public abstract class ExpandedStorageHandler {
     public static Collection<BlockPos> check(BlockState state, World world, BlockPos pos) {
-        if (state.getBlock() instanceof ChestBlock) {
-            if (state.get(ChestBlock.TYPE) == CursedChestType.SINGLE) return Collections.emptyList();
-            else return Collections.singleton(pos.offset(ChestBlock.getDirectionToAttached(state)));
+        if (state.getBlock() instanceof AbstractChestBlock) {
+            if (state.get(AbstractChestBlock.CURSED_CHEST_TYPE) == CursedChestType.SINGLE) return Collections.emptyList();
+            else return Collections.singleton(pos.offset(AbstractChestBlock.getDirectionToAttached(state)));
         } else {
             return Collections.emptyList();
         }
