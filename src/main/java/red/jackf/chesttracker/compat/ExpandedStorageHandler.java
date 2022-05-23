62cc340d7ebc01708a2531584321e77ac43f308c
Jack Pearce <jack@jackf.red>
2022-05-23 19:32:01 +0100

    Stop using ExpandedStorage internals, use given APIs instead

--- src/main/java/red/jackf/chesttracker/compat/ExpandedStorageHandler.java
+++ src/main/java/red/jackf/chesttracker/compat/ExpandedStorageHandler.java
@@ -3,19 +3,18 @@ package red.jackf.chesttracker.compat;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import ninjaphenix.expandedstorage.api.ExpandedStorageAccessors;
 import ninjaphenix.expandedstorage.block.AbstractChestBlock;
 import ninjaphenix.expandedstorage.block.misc.CursedChestType;
 
 import java.util.Collection;
 import java.util.Collections;
 
-@SuppressWarnings("UnstableApiUsage")
 public abstract class ExpandedStorageHandler {
     public static Collection<BlockPos> check(BlockState state, World world, BlockPos pos) {
-        if (state.getBlock() instanceof AbstractChestBlock) {
-            if (state.get(AbstractChestBlock.CURSED_CHEST_TYPE) == CursedChestType.SINGLE)
-                return Collections.emptyList();
-            else return Collections.singleton(pos.offset(AbstractChestBlock.getDirectionToAttached(state)));
+        var attachedDirection = ExpandedStorageAccessors.getAttachedChestDirection(state);
+        if (attachedDirection.isPresent()) {
+            return Collections.singleton(pos.offset(attachedDirection.get()));
         } else {
             return Collections.emptyList();
         }
