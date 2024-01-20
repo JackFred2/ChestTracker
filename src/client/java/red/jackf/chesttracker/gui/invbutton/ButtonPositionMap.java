9fe50f69bf797643bd8dde4c859515d7f3a0380a
Jack Pearce <jack@jackf.red>
2024-01-20 03:09:17 +0000

    [feat] persistence on custom positions

--- src/client/java/red/jackf/chesttracker/gui/invbutton/ButtonPositionMap.java
+++ src/client/java/red/jackf/chesttracker/gui/invbutton/ButtonPositionMap.java
@@ -1,14 +1,23 @@
 package red.jackf.chesttracker.gui.invbutton;
 
+import com.mojang.serialization.Codec;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import red.jackf.chesttracker.util.Constants;
+import red.jackf.chesttracker.util.FileUtil;
+import red.jackf.jackfredlib.api.base.codecs.JFLCodecs;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ButtonPositionMap {
     private static final ButtonPosition FALLBACK_DEFAULT
             = new ButtonPosition(ButtonPosition.HorizontalAlignment.right, 14, ButtonPosition.VerticalAlignment.top, 5);
+    private static final Path USER_PATH = Constants.STORAGE_DIR.resolve("user_button_positions.dat");
+    private static final Codec<Map<String, ButtonPosition>> USER_CODEC = JFLCodecs.mutableMap(Codec.unboundedMap(
+            Codec.STRING, ButtonPosition.CODEC
+    ));
 
     private static final Map<String, ButtonPosition> datapackPositions = new HashMap<>();
     private static final Map<String, ButtonPosition> userPositions = new HashMap<>();
@@ -16,6 +25,12 @@ public class ButtonPositionMap {
     public static void loadDatapackPositions(Map<String, ButtonPosition> datapack) {
         datapackPositions.clear();
         datapackPositions.putAll(datapack);
+    }
+
+    public static void loadUserPositions() {
+        userPositions.clear();
+
+        FileUtil.loadFromNbt(USER_CODEC, USER_PATH).ifPresent(userPositions::putAll);
     }
 
     private static String getClassString(AbstractContainerScreen<?> screen) {
@@ -35,6 +50,8 @@ public class ButtonPositionMap {
         String className = getClassString(screen);
 
         userPositions.put(className, position);
+
+        FileUtil.saveToNbt(userPositions, USER_CODEC, USER_PATH);
     }
 
     public static ButtonPosition getFor(AbstractContainerScreen<?> screen) {
@@ -51,5 +68,4 @@ public class ButtonPositionMap {
         // return the default, or in case nothing's loaded the fallback
         return datapackPositions.getOrDefault("DEFAULT", FALLBACK_DEFAULT);
     }
-
 }
