99480ab0ae64c8f835c6253d5051107a5f2f81cc
Jack Pearce <jack@jackf.red>
2024-04-14 04:24:26 +0100

    [internal] slight doc change for button positions

--- src/client/java/red/jackf/chesttracker/api/package-info.java
+++ src/client/java/red/jackf/chesttracker/api/package-info.java
@@ -1,6 +1,9 @@
 /**
  * <p>Master API classes for ChestTracker.</p>
  *
+ * <p>You can also supply custom default positions for the in-game button; these are done in datapack files. See the
+ * built-in files as an example, or the GitHub wiki for more information</p>
+ *
  * <p>
  * NOTE: These APIs are experimental; they may possibly change in the future.
  * </p>
