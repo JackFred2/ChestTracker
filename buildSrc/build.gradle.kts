c55d28cffbec51b365f6a6b9eb33368ed8671af2
Jack Pearce <jack@jackf.red>
2024-04-24 19:05:11 +0100

    [internal] change buildscript for java 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 repositories {
