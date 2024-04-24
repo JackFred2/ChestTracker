c9aa6ceac421cbf12697d5b5cfc55e4a810e05ff
Jack Pearce <jack@jackf.red>
2024-04-24 19:05:11 +0100

    [internal] change buildscript for java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import red.jackf.UpdateDependenciesTask
 
 plugins {
 	id("maven-publish")
-	id("fabric-loom") version "1.5-SNAPSHOT"
+	id("fabric-loom") version "1.6-SNAPSHOT"
 	id("com.github.breadmoirai.github-release") version "2.4.1"
 	id("org.ajoberstar.grgit") version "5.2.1"
 	id("me.modmuss50.mod-publish-plugin") version "0.3.3"
@@ -140,9 +140,6 @@ repositories {
 
 java {
 	withSourcesJar()
-
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
 }
 
 loom {
