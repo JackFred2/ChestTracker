877869faabe7f27c651e3ba5a912d33b9d396295
Jack Pearce <jack@jackf.red>
2024-05-07 03:19:45 +0100

    [feat] hypixel smp

--- src/client/java/red/jackf/chesttracker/impl/compat/servers/hypixel/HypixelProvider.java
+++ src/client/java/red/jackf/chesttracker/impl/compat/servers/hypixel/HypixelProvider.java
@@ -1,8 +1,11 @@
 package red.jackf.chesttracker.impl.compat.servers.hypixel;
 
 import com.google.common.collect.Streams;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -15,8 +18,10 @@ import red.jackf.chesttracker.api.memory.MemoryBankAccess;
 import red.jackf.chesttracker.api.providers.*;
 import red.jackf.chesttracker.api.providers.context.ScreenCloseContext;
 import red.jackf.chesttracker.api.providers.context.ScreenOpenContext;
+import red.jackf.chesttracker.api.providers.defaults.DefaultProvider;
 import red.jackf.chesttracker.impl.ChestTracker;
 import red.jackf.jackfredlib.client.api.gps.Coordinate;
+import red.jackf.jackfredlib.client.api.gps.ScoreboardSnapshot;
 import red.jackf.whereisit.api.search.ConnectedBlocksGrabber;
 
 import java.util.List;
@@ -32,8 +37,11 @@ public class HypixelProvider extends ServerProvider {
     public static final ResourceLocation SKYBLOCK_ENDER_CHEST = new ResourceLocation("hypixel", "skyblock_ender_chest");
 
     private static final List<MemoryKeyIcon> ICONS = Streams.concat(Stream.of(
-            new MemoryKeyIcon(SKYBLOCK_PRIVATE_ISLAND, Items.OAK_SAPLING.getDefaultInstance())
+            new MemoryKeyIcon(SKYBLOCK_PRIVATE_ISLAND, Items.OAK_SAPLING.getDefaultInstance()),
+            new MemoryKeyIcon(SKYBLOCK_ENDER_CHEST, Items.ENDER_CHEST.getDefaultInstance())
     ), ProviderUtils.getDefaultIcons().stream()).toList();
+
+    private boolean isOnSMP = false;
 
     @Override
     public ResourceLocation id() {
@@ -49,6 +57,25 @@ public class HypixelProvider extends ServerProvider {
     @Override
     public List<MemoryKeyIcon> getMemoryKeyIcons() {
         return ICONS;
+    }
+
+    // Detects joining SMP servers.
+    @Override
+    public void onGameMessageReceived(Component message, boolean isOverlay) {
+        if (SMP.isSMPJoinMessage(message)) {
+            this.isOnSMP = true;
+        }
+    }
+
+    // Detects leaving SMP servers.
+    // All non-SMP client states seem to have www.hypixel.net at the bottom of the scoreboard.
+    @Override
+    public void onRespawn(ResourceKey<Level> from, ResourceKey<Level> to) {
+        Optional<ScoreboardSnapshot> scoreboard = ScoreboardSnapshot.take();
+        if (scoreboard.isEmpty()) return;
+        Optional<Pair<Component, Component>> lastRow = scoreboard.get().entryFromBottom(0);
+        if (lastRow.isEmpty()) return;
+        if (lastRow.get().getFirst().getString().contains("www.hypixel.net")) this.isOnSMP = false;
     }
 
     @Override
@@ -95,6 +122,8 @@ public class HypixelProvider extends ServerProvider {
                     bank.addMemory(SKYBLOCK_ENDER_CHEST, new BlockPos(page.get(), 0, 0), memory);
                 }
             }
+        } else if (this.isOnSMP) {
+            DefaultProvider.INSTANCE.onScreenClose(context);
         }
     }
 
@@ -102,6 +131,8 @@ public class HypixelProvider extends ServerProvider {
     public Optional<MemoryLocation> getMemoryLocation(ClientBlockSource cbs) {
         if (Skyblock.isOnPrivateIsland()) {
             return Optional.of(MemoryLocation.inWorld(SKYBLOCK_PRIVATE_ISLAND, cbs.pos()));
+        } else if (this.isOnSMP) {
+            return DefaultProvider.INSTANCE.getMemoryLocation(cbs);
         }
         return Optional.empty();
     }
@@ -110,6 +141,8 @@ public class HypixelProvider extends ServerProvider {
     public Optional<ResourceLocation> getPlayersCurrentKey(Level level, LocalPlayer player) {
         if (Skyblock.isOnPrivateIsland()) {
             return Optional.of(SKYBLOCK_PRIVATE_ISLAND);
+        } else if (this.isOnSMP) {
+            return DefaultProvider.INSTANCE.getPlayersCurrentKey(level, player);
         }
 
         return Optional.empty();
@@ -120,6 +153,8 @@ public class HypixelProvider extends ServerProvider {
         if (Skyblock.isPlayerOn()) {
             reciever.accept("On Skyblock");
             reciever.accept("Area: " + Skyblock.getArea().orElse("<unknown>"));
+        } else if (isOnSMP) {
+            reciever.accept("On SMP");
         }
     }
 }
