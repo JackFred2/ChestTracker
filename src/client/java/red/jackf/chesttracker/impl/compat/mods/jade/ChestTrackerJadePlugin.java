da456367f27148cce4e1ef109c4f9cbf4ca1d861
Jack Pearce <jack@jackf.red>
2024-08-14 18:35:04 +0100

    [fix] #197; also remove show mod name option

--- src/client/java/red/jackf/chesttracker/impl/compat/mods/jade/ChestTrackerJadePlugin.java
+++ src/client/java/red/jackf/chesttracker/impl/compat/mods/jade/ChestTrackerJadePlugin.java
@@ -1,19 +1,13 @@
 package red.jackf.chesttracker.impl.compat.mods.jade;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import red.jackf.chesttracker.impl.ChestTracker;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaPlugin;
 
 public class ChestTrackerJadePlugin implements IWailaPlugin {
-    protected static final ResourceLocation CONFIG_SHOW_TEXT = ChestTracker.id("show_mod_name");
-
     @Override
     public void registerClient(IWailaClientRegistration registration) {
         registration.registerBlockComponent(JadeClientContentsPreview.INSTANCE, Block.class);
         registration.markAsClientFeature(JadeClientContentsPreview.ID);
-        registration.addConfig(CONFIG_SHOW_TEXT, false);
-        registration.markAsClientFeature(CONFIG_SHOW_TEXT);
     }
 }
--- src/client/java/red/jackf/chesttracker/impl/compat/mods/jade/JadeClientContentsPreview.java
+++ src/client/java/red/jackf/chesttracker/impl/compat/mods/jade/JadeClientContentsPreview.java
@@ -1,6 +1,5 @@
 package red.jackf.chesttracker.impl.compat.mods.jade;
 
-import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -24,58 +23,55 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static net.minecraft.network.chat.Component.translatable;
-
-public enum JadeClientContentsPreview implements IBlockComponentProvider {
-    INSTANCE;
+public class JadeClientContentsPreview implements IBlockComponentProvider {
+    public static JadeClientContentsPreview INSTANCE = new JadeClientContentsPreview();
+    private JadeClientContentsPreview() {}
 
     public static final ResourceLocation ID = ChestTracker.id("memory_preview");
 
-    @Override
-    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+    private static void possiblyAddItems(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config, Memory memory) {
         if (config.get(Identifiers.UNIVERSAL_ITEM_STORAGE) && accessor.getServerData().contains("JadeItemStorage"))
             return; // don't do it if jade is handling it
         if (config.get(Identifiers.MC_FURNACE)
                 && accessor.getBlock() instanceof AbstractFurnaceBlock
                 && accessor.getServerData()
                 .contains("furnace", Tag.TAG_LIST))
-            return; // don't do furnaces if done
+            return; // don't do furnaces if handled so progress still shows
 
+        var stacks = ItemStacks.flattenStacks(memory.items(), true);
+
+        int max = config.getInt(accessor.showDetails() ? Identifiers.UNIVERSAL_ITEM_STORAGE_DETAILED_AMOUNT : Identifiers.UNIVERSAL_ITEM_STORAGE_NORMAL_AMOUNT);
+        int perLine = config.getInt(Identifiers.UNIVERSAL_ITEM_STORAGE_ITEMS_PER_LINE);
+
+        List<List<IElement>> lines = new ArrayList<>();
+        List<IElement> currentLine = new ArrayList<>(perLine);
+        for (int i = 0; i < max && i < stacks.size(); i++) {
+            ItemStack item = stacks.get(i);
+            currentLine.add(IElementHelper.get().item(item));
+            if (currentLine.size() == perLine) {
+                lines.add(currentLine);
+                currentLine = new ArrayList<>(perLine);
+            }
+        }
+        if (!currentLine.isEmpty()) lines.add(currentLine);
+        for (int i = 0; i < lines.size(); i++) {
+            tooltip.add(lines.get(i));
+            if (i < lines.size() - 1) tooltip.setLineMargin(-1, ScreenDirection.DOWN, -1);
+        }
+    }
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
         MemoryBankAccess.INSTANCE.getLoaded().ifPresent(bank -> {
-            // TODO ServerProvider#getMemoryKeyOverride and for WTHIT too
             Optional<Memory> memory = bank.getMemory(accessor.getLevel(), accessor.getPosition());
             if (memory.isEmpty()) return;
 
-            var stacks = ItemStacks.flattenStacks(memory.get().items(), true);
-
-            int max = config.getInt(accessor.showDetails() ? Identifiers.UNIVERSAL_ITEM_STORAGE_DETAILED_AMOUNT : Identifiers.UNIVERSAL_ITEM_STORAGE_NORMAL_AMOUNT);
-            int perLine = config.getInt(Identifiers.UNIVERSAL_ITEM_STORAGE_ITEMS_PER_LINE);
-
-            List<List<IElement>> lines = new ArrayList<>();
-            List<IElement> currentLine = new ArrayList<>(perLine);
-            for (int i = 0; i < max && i < stacks.size(); i++) {
-                ItemStack item = stacks.get(i);
-                currentLine.add(IElementHelper.get().item(item));
-                if (currentLine.size() == perLine) {
-                    lines.add(currentLine);
-                    currentLine = new ArrayList<>(perLine);
-                }
-            }
-            if (!currentLine.isEmpty()) lines.add(currentLine);
-            for (int i = 0; i < lines.size(); i++) {
-                tooltip.add(lines.get(i));
-                if (i < lines.size() - 1) tooltip.setLineMargin(-1, ScreenDirection.DOWN, -1);
-            }
+            // items
+            possiblyAddItems(tooltip, accessor, config, memory.get());
 
             Component name = memory.get().renderName();
             if (name != null) {
                 tooltip.replace(Identifiers.CORE_OBJECT_NAME, IThemeHelper.get().title(name));
-            }
-
-            if (accessor.showDetails() && config.get(ChestTrackerJadePlugin.CONFIG_SHOW_TEXT)) {
-                tooltip.add(
-                        translatable("chesttracker.compatibility.brand", translatable("chesttracker.title").withStyle(ChatFormatting.GOLD)).withStyle(ChatFormatting.GRAY)
-                );
             }
         });
     }
